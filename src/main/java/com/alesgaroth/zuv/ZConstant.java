4d0f91c04779eb30c4016a3e58dd7c0a3dda82b6
Alan Linton <alesgaroth@gmail.com>
2023-06-27 18:17:29 -0400

    adding values

--- src/main/java/com/alesgaroth/zuv/ZConstant.java
+++ src/main/java/com/alesgaroth/zuv/ZConstant.java
@@ -3,6 +3,6 @@ package com.alesgaroth.zuv;
 public class ZConstant<T> extends ZNode<T> {
 
   public ZConstant(T value) {
-  	this.value = new ZValue<T>(value);
+  	this.value.set(value);
   }
 }
--- src/main/java/com/alesgaroth/zuv/ZValue.java
+++ src/main/java/com/alesgaroth/zuv/ZValue.java
@@ -6,6 +6,10 @@ public class ZValue<T> {
   	this.value = value;
   }
 
+  public void set(T value) {
+  	this.value = value;
+  }
+
   public void addListener(ZListener<T> o) {
   	if (o == null) throw new NullPointerException("null listeners are forbidden");
   }
