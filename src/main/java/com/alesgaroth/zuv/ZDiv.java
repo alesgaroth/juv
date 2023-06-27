a6d40d23de77e06806544ea8adb9bc2ba7380cdb
Alan Linton <alesgaroth@gmail.com>
2023-06-27 19:34:43 -0400

    Can divide

--- src/main/java/com/alesgaroth/zuv/ZDiv.java
+++ src/main/java/com/alesgaroth/zuv/ZDiv.java
@@ -1,10 +1,18 @@
 package com.alesgaroth.zuv;
 
-public class ZDiv<T> extends ZNode<T> {
+public class ZDiv extends ZNode<Integer> {
+
+  ZValue<Integer> [] inputs = new ZValue[2];
+
   public ZDiv() {
+  	this.value.set((Integer)8);
   }
 
   void setInput(ZValue v, int input) {
+	inputs[input] = v;
+	if (inputs[0] != null && inputs[1] != null) {
+		this.value.set((Integer)(((int)inputs[0].fetch())/(inputs[1].fetch())));
+	}
   }
 
 }
--- src/test/java/com/alesgaroth/zuv/ZDivTest.java
+++ src/test/java/com/alesgaroth/zuv/ZDivTest.java
@@ -6,15 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ZDivTest {
 
-  ZDiv<Integer> div;
+  ZDiv div;
   ZVar<Integer> var1;
   ZVar<Integer> var2;
 
   @Test public void canCreateDiv() {
-    div = new ZDiv<Integer>();
+    div = new ZDiv();
 	var1 = new ZVar<Integer>(32);
 	var2 = new ZVar<Integer>(4);
 	div.setInput(var1.output(0), 0);
 	div.setInput(var2.output(0), 1);
+
+	assertEquals((Integer)8, div.output(0).fetch());
+  }
+
+  @Test public void canChangeDiv() {
+    div = new ZDiv();
+	var1 = new ZVar<Integer>(40);
+	var2 = new ZVar<Integer>(4);
+	div.setInput(var1.output(0), 0);
+	div.setInput(var2.output(0), 1);
+
+	assertEquals((Integer)10, div.output(0).fetch());
   }
 }
