32ac0c1535a9cccbfc74cde2c03319108cd0681f
Alan Linton <alesgaroth@gmail.com>
2023-06-27 18:26:20 -0400

    listeners!

--- src/test/java/com/alesgaroth/zuv/ZVarTest.java
+++ src/test/java/com/alesgaroth/zuv/ZVarTest.java
@@ -6,20 +6,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ZVarTest {
 
-  boolean listenerCalled = false;
+  boolean listener1Called = false;
+  boolean listener2Called = false;
 
-  @Test public void canCreateVar(){
+  @Test public void canListen(){
 	ZVar<Integer> var;
     var = new ZVar<Integer>((Integer)7);
 	ZListener<Integer> listener = new ZListener<>(){
 	  @Override
 	  public void valueChanged() {
-	  listenerCalled = true;
+	  listener1Called = true;
 	  }
 	};
 	var.output(0).addListener(listener);
 	var.set((Integer)8);
-	assertTrue(listenerCalled);
-	
+	assertTrue(listener1Called);
+  }
+
+  @Test public void canHaveTwoListeners(){
+	ZVar<Integer> var;
+    var = new ZVar<Integer>((Integer)7);
+	ZListener<Integer> listener = new ZListener<>(){
+	  @Override
+	  public void valueChanged() {
+	  listener1Called = true;
+	  }
+	};
+	ZListener<Integer> listener2 = new ZListener<>(){
+	  @Override
+	  public void valueChanged() {
+	  listener2Called = true;
+	  }
+	};
+	var.output(0).addListener(listener);
+	var.output(0).addListener(listener2);
+	var.set((Integer)8);
+	assertTrue(listener1Called);
+	assertTrue(listener2Called);
   }
 }
