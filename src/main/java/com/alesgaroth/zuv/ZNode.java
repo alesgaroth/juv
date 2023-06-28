cd625bdc69e5cea0d65f9419f09a6a419702c235
Alan Linton <alesgaroth@gmail.com>
2023-06-28 07:19:49 -0400

    graphs can have inputs

--- src/main/java/com/alesgaroth/zuv/ZNode.java
+++ src/main/java/com/alesgaroth/zuv/ZNode.java
@@ -2,6 +2,7 @@ package com.alesgaroth.zuv;
 
 public class ZNode<T> {
   protected ZValue<T> value;
+  ZValue<T> input;
 
   protected ZNode() {
   	value = new ZValue<T>(null);
@@ -13,5 +14,9 @@ public class ZNode<T> {
 
   public ZValue<T> output(int outputNum) {
   	return value;
+  }
+
+  void setInput(ZValue<T> v, int input) {
+	this.input = v;
   }
 }
