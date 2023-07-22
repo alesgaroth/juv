0d186ddfce34b94edeee57ecd0ab50b9a291e88a
Alan <alesgaroth@gmail.com>
2023-07-22 18:08:33 +0000

    more queue tests

--- src/main/java/com/alesgaroth/zuv/ZQueue.java
+++ src/main/java/com/alesgaroth/zuv/ZQueue.java
@@ -1,15 +1,22 @@
 package com.alesgaroth.zuv;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class ZQueue {
 
-    ZNode<?> stack;
+    Deque<ZNode<?>> deque = new LinkedList<>();
 
     public <T> void doLater(ZNode<T> zNode) {
-        stack = zNode;
+        deque.add(zNode);
     }
 
     public ZNode<?> next() {
-        return stack;
+        return deque.remove();
     }
 
+    public void prepend(ZNode<?> n) {
+        deque.addFirst(n);
+    }
 }
--- src/test/java/com/alesgaroth/zuv/ZQueueTest.java
+++ src/test/java/com/alesgaroth/zuv/ZQueueTest.java
@@ -17,4 +17,41 @@ public class ZQueueTest {
         q.doLater(n);
         assertSame(n, q.next(), "Empty Queue push/pop should return same.");
     }
+
+    @Test
+    void canPutTwothingsInQueue() {
+        ZNode<Integer> n = new ZNode<Integer>();
+        ZNode<Integer> m = new ZNode<Integer>();
+        q.doLater(n);
+        q.doLater(m);
+        assertSame(n, q.next(), "First in queue is first out");
+        assertSame(m, q.next(), "Second in queue is second out");
+    }
+
+    @Test
+    void canPutBack() {
+       ZNode<Integer> n = new ZNode<Integer>();
+       ZNode<Integer> m = new ZNode<Integer>();
+       q.doLater(n);
+       q.doLater(m);
+       assertSame(n, q.next(), "First in queue is first out");
+       q.prepend(n);
+       assertSame(n, q.next(), "First put back is first out");
+    }
+
+    @Test
+    void canPrepend() {
+       ZNode<Integer> n = new ZNode<Integer>();
+       ZNode<Integer> m = new ZNode<Integer>();
+       ZNode<Integer> i = new ZNode<Integer>();
+       q.doLater(n);
+       q.doLater(m);
+       assertSame(n, q.next(), "First in queue is first out");
+       q.prepend(n);
+       q.prepend(i);
+       assertSame(i, q.next(), "Second put back is first out");
+       assertSame(n, q.next(), "First put back is second out");
+       assertSame(m, q.next(), "last added to end is last out");
+    }
+
 }
