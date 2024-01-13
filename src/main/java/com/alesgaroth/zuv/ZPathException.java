f62187e8114faddccd7fe49d4007ab473e3de72f
Alan Linton <alesgaroth@gmail.com>
2024-01-13 16:44:44 -0500

    add exception used in previous commit

---
+++ src/main/java/com/alesgaroth/zuv/ZPathException.java
@@ -0,0 +1,8 @@
+package com.alesgaroth.zuv;
+
+public class ZPathException extends RuntimeException {
+  public static final long serialVersionUID = 1L;
+  public ZPathException(String message) {
+    super(message);
+  }
+}
