e944429a64d35693b27e1f7fc17fd4d99b6c7579
Alan Linton <alesgaroth@gmail.com>
2025-01-27 20:06:38 -0500

    CompositeListener setup

--- index/src/main/java/com/alesgaroth/zuv/index/CompositeListener.java
+++ index/src/main/java/com/alesgaroth/zuv/index/CompositeListener.java
@@ -1,12 +1,39 @@
 package com.alesgaroth.zuv.index;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
-public class CompositeListener {
+public class CompositeListener implements CRDT.CRDTListener<String> {
+  Map<String, CRDT.CRDTListener<String>> listeners = new HashMap<>();
   public CompositeListener(CRDT<String> crdt) {
   }
 
   public void register(CRDT.CRDTListener<String> l, String prefix) {
+    listeners.put(prefix, l);
+  }
+
+  public static String getPrefixFrom(String e) {
+    if (e != null) {
+      if (e.equals("/")) {
+        return "/";
+      }
+      if (e.charAt(e.length() - 1) != '/') {
+        return e.substring(0, e.lastIndexOf('/') + 1);
+      } else {
+        return e.substring(0, e.lastIndexOf('/', e.length() - 2) + 1);
+      }
+    }
+    return e;
+  }
+
+
+  public void added(String e) {
+    listeners.get(getPrefixFrom(e)).added(e);
+  }
+  public void removed(String e) {
+    listeners.get(getPrefixFrom(e)).removed(e);
   }
 }
--- index/src/test/java/com/alesgaroth/zuv/index/CompositeListenerTest.java
+++ index/src/test/java/com/alesgaroth/zuv/index/CompositeListenerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -12,23 +13,66 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
 public class CompositeListenerTest {
   public class SpyListener implements CRDT.CRDTListener<String> {
+    public ArrayList<String> log = new ArrayList<>();
     public void added(String e) {
+      log.add("added " + e);
     }
     public void removed(String e) {
+      log.add("removed " + e);
     }
   }
+
   @Test
   public void canAddListener() {
     CRDT fake = new FakeCRDT();
     CompositeListener cl = new CompositeListener(fake);
+    SpyListener spy = new SpyListener();
+    cl.register(spy, "/");
+    fake.setListener(cl);
+    fake.add("/bob");
+    assertTrue(spy.log.contains("added /bob"));
+    fake.remove("/bob");
+    assertTrue(spy.log.contains("removed /bob"));
+  }
 
-    cl.register(new SpyListener(), "/");
+  @Test
+  public void calcPrefix() {
+     assertNull(CompositeListener.getPrefixFrom(null));
+     assertEquals("/", CompositeListener.getPrefixFrom("/"));
+     assertEquals("/", CompositeListener.getPrefixFrom("/bob"));
+     assertEquals("/", CompositeListener.getPrefixFrom("/bob/"));
+     assertEquals("/bob/", CompositeListener.getPrefixFrom("/bob/foo"));
+     assertEquals("/bob/", CompositeListener.getPrefixFrom("/bob/foo/"));
+  }
+
+  @Test
+  public void canAdd2Listeners() {
+    CRDT fake = new FakeCRDT();
+    CompositeListener cl = new CompositeListener(fake);
+    SpyListener spy = new SpyListener();
+    SpyListener spy2 = new SpyListener();
+    cl.register(spy, "/");
+    cl.register(spy2, "/bob/");
+    fake.setListener(cl);
+    fake.add("/bob/");
+    assertTrue(spy.log.contains("added /bob/"));
+    assertFalse(spy2.log.contains("added /bob/"));
+    fake.remove("/bob/");
+    assertTrue(spy.log.contains("removed /bob/"));
+    assertFalse(spy2.log.contains("removed /bob/"));
+    fake.add("/bob/foo/");
+    assertTrue(spy2.log.contains("added /bob/foo/"));
+    assertFalse(spy.log.contains("added /bob/foo/"));
+    fake.remove("/bob/foo/");
+    assertTrue(spy2.log.contains("removed /bob/foo/"));
+    assertFalse(spy.log.contains("removed /bob/foo/"));
   }
 
 }
